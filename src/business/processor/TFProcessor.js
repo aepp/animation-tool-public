e1758fcdd01280ef22e3cc7f257ca2c8b3cf8e03
Aleksandr Epp <aleksandr.epp@gmail.com>
2022-02-23 11:53:44 +0100

    Filter frames with score below specified threshold for selected model (hard coded for now)

--- src/business/processor/TFProcessor.js
+++ src/business/processor/TFProcessor.js
@@ -44,15 +44,26 @@ export class TFProcessor extends IDataSetProcessor {
     this.calculateNormalScaleFactor2D();
     this.calculateTranslations();
 
-    const framesPerPerson = this.frames.map(persons =>
-      persons.map(({keypoints, score}) => ({
-        keyPoints: keypoints.map(point => ({
-          ...this.getNormalizedCenteredPoint(point),
-          z: 0
-        })),
-        score
-      }))
-    );
+    const framesPerPerson = this.frames
+      .map(frame =>
+        frame.reduce((framesPerPersonReduced, personsFrame) => {
+          if (personsFrame.score >= ScoreThreshHold[this._model]) {
+            framesPerPersonReduced.push(personsFrame);
+          }
+          return framesPerPersonReduced;
+        }, [])
+      )
+      .filter(frame => frame.length)
+      .map(frame =>
+        frame.map(({keypoints, score}) => ({
+          keyPoints: keypoints.map(point => ({
+            ...this.getNormalizedCenteredPoint(point),
+            z: 0
+          })),
+          score
+        }))
+      );
+
     return new Promise(resolve => {
       /**
        * @type {PreProcessedDataSet}
