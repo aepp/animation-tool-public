eaadf8558583c14eaddb8fb52aa97d795107703f
Aleksandr Epp <aleksandr.epp@gmail.com>
2022-04-15 14:21:27 +0200

    Add animation playback toggling with the space key

--- src/react/views/Visualization/modules/AnimationControls/components/PlayButton/index.jsx
+++ src/react/views/Visualization/modules/AnimationControls/components/PlayButton/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {IconButton} from '@mui/material';
 import {
@@ -13,6 +13,18 @@ export const PlayButton = () => {
   const dispatch = useDispatch();
   const isPlaying = useSelector(selectIsPlaying);
   const isInitialized = useSelector(selectIsAnimationInitialized);
+  const [spaceTogglingRegistered, registerSpaceToggling] = useState(false);
+
+  useEffect(() => {
+    if (!spaceTogglingRegistered) {
+      window.addEventListener('keyup', e => {
+        if (e.key === ' ' || e.code === 'Space') {
+          dispatch(togglePlay());
+        }
+      });
+      registerSpaceToggling(true);
+    }
+  }, [spaceTogglingRegistered, registerSpaceToggling, dispatch]);
 
   return (
     <IconButton
