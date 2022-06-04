e41a7029bcc8de9a55a2b7a5102f518192ab358d
Aleksandr Epp <aleksandr.epp@gmail.com>
2022-06-04 16:15:26 +0200

    Remove deployment

--- docker-cmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-yarn start-server
-if [ '(tar -cf - /usr/local/share/.cache/yarn/v2 | crc32)' != '(tar -cf - .yarn_cache | crc32)' ]; then
-    cp -r /usr/local/share/.cache/yarn/v2/. .yarn_cache/;
-fi
-
-exec "$@"
